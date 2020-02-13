ee6b0e9e1868fecfa3e0275fef2cc64c7625dc94
Ivan Melnychenko <liftiwan1996@gmail.com>
2020-02-13 16:21:12 +0200

    first commit

---
+++ RegistrationForm/src/main/java/view/TextValue.java
@@ -0,0 +1,10 @@
+package view;
+
+public interface TextValue {
+
+  String INPUT_ORDER_COUNTRY = "If your language English, please input \"en\", а якщо Ваша рідна мова українська введіть \"ua\"";
+  String WRONG_MESSAGE_INPUT_ORDER_COUNTRY = "Choose \"en\" or \"ua\"";
+String INPUT_NAME = "name";
+  String INPUT_SURNAME = "surname";
+  String WRONG_INPUT = "error";
+}

f30a878026a01b516658cce41544e1268e6f3a35
Ivan Melnychenko <liftiwan1996@gmail.com>
2020-02-16 23:48:52 +0200

    change main functional

---
+++ RegistrationForm/src/main/java/model/Person.java
@@ -0,0 +1,93 @@
+package model;
+
+public class Person {
+
+  private String name;
+  private String surname;
+  private String nickname;
+  private String phone_Nmb;
+  private String phone_Nmb_House;
+  private String index;
+  private String city;
+  private String street;
+  private String number_Of_Flat;
+  private String address;
+  private String email;
+  private String house_Number;
+
+  public String getHouse_Number() {
+    return house_Number;
+  }
+
+  public void setHouse_Number(String house_Number) {
+    this.house_Number = house_Number;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
+
+  public String getNumber_Of_Flat() {
+    return number_Of_Flat;
+  }
+
+  public void setNickname(String nickname) {
+    this.nickname = nickname;
+  }
+
+  public void setPhone_Nmb(String phone_Nmb) {
+    this.phone_Nmb = phone_Nmb;
+  }
+
+  public void setPhone_Nmb_House(String phone_Nmb_House) {
+    this.phone_Nmb_House = phone_Nmb_House;
+  }
+
+  public String getIndex() {
+    return index;
+  }
+
+  public void setIndex(String index) {
+    this.index = index;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  public void setStreet(String street) {
+    this.street = street;
+  }
+
+  public void setNumber_Of_Flat(String number_Of_Flat) {
+    this.number_Of_Flat = number_Of_Flat;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+}

aaab2df032bc47da917aeb7bbb606852f024f620
Ivan Melnychenko <liftiwan1996@gmail.com>
2020-02-16 23:47:03 +0200

    change main functional

--- RegistrationForm/src/main/java/Main.java
+++ RegistrationForm/src/main/java/Main.java
@@ -1,13 +1,14 @@
 import controller.Controller;
+import model.Model;
 import view.View;
 
 public class Main {
 
   public static void main(String[] args) {
+    Model model = new Model();
     View view = new View();
-    Controller controller = new Controller(view);
+    Controller controller = new Controller(view, model);
     controller.getUser();
 
   }
-
 }

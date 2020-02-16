7e9a2dcbd646503b24da6b35dd929f982da2edfb
Ivan Melnychenko <liftiwan1996@gmail.com>
2020-02-16 23:49:05 +0200

    change main functional

--- RegistrationForm/src/main/java/model/Model.java
+++ RegistrationForm/src/main/java/model/Model.java
@@ -2,4 +2,16 @@ package model;
 
 public class Model {
 
+  private Person person;
+  private InputNoteBook inputNoteBook = new InputNoteBook();
+
+  public Person createPerson() {
+    person = new Person();
+    inputNoteBook.getListPerson().add(person);
+    return person;
+  }
+
+  public Person getPerson() {
+    return person;
+  }
 }

c7e2e1076682fa495e39321eb2ab305ecb4706f1
Ivan Melnychenko <liftiwan1996@gmail.com>
2020-02-16 23:46:49 +0200

    change main functional

--- RegistrationForm/src/main/java/model/InputNoteBook.java
+++ RegistrationForm/src/main/java/model/InputNoteBook.java
@@ -1,5 +1,17 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputNoteBook {
 
+  private List<Person> listPerson = new ArrayList<>();
+
+  public List<Person> getListPerson() {
+    return listPerson;
+  }
+
+  public void setListPerson(List<Person> listPerson) {
+    this.listPerson = listPerson;
+  }
 }

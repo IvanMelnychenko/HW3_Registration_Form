2ce96eb4ad9a6089a5bf89c146be033052c90cdf
Ivan Melnychenko <liftiwan1996@gmail.com>
2020-02-16 23:48:22 +0200

    change main functional

--- RegistrationForm/src/main/java/view/TextValue.java
+++ RegistrationForm/src/main/java/view/TextValue.java
@@ -3,8 +3,16 @@ package view;
 public interface TextValue {
 
   String INPUT_ORDER_COUNTRY = "If your language English, please input \"en\", а якщо Ваша рідна мова українська введіть \"ua\"";
-  String WRONG_MESSAGE_INPUT_ORDER_COUNTRY = "Choose \"en\" or \"ua\"";
-String INPUT_NAME = "name";
-  String INPUT_SURNAME = "surname";
-  String WRONG_INPUT = "error";
+  String INPUT_NAME = "input.name";
+  String INPUT_SURNAME = "input.surname";
+  String INPUT_NICKNAME = "input.nickname";
+  String INPUT_EMAIL = "input.email";
+  String INPUT_PHONE_NUMBER_HOME = "input.phone.number.home";
+  String INPUT_PHONE_NUMBER = "input.phone.number";
+  String INPUT_INDEX = "input.index";
+  String INPUT_CITY = "input.city";
+  String INPUT_STREET = "input.street";
+  String INPUT_HOUSE_NUMBER = "input.house.number";
+  String INPUT_FLAT = "input.flat";
+  String WRONG_INPUT = "wrong.input";
 }

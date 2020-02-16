253e83e59817ce5a547ccd9e2a295925893008a0
Ivan Melnychenko <liftiwan1996@gmail.com>
2020-02-16 23:48:09 +0200

    change main functional

--- RegistrationForm/src/main/java/controller/UtilityController.java
+++ RegistrationForm/src/main/java/controller/UtilityController.java
@@ -1,5 +1,25 @@
 package controller;
 
+import java.util.Scanner;
+import view.TextValue;
+import view.View;
+
 public class UtilityController {
 
+  private Scanner scanner;
+  private View view;
+
+  public UtilityController(Scanner scanner, View view) {
+    this.scanner = scanner;
+    this.view = view;
+  }
+
+  public String inputStringValueWithScanner(String message, String regex) {
+    String res;
+    view.printStringInput(message);
+    while (!(scanner.hasNext() && (res = scanner.next()).matches(regex))) {
+      view.printWrongStringInput(TextValue.WRONG_INPUT, message);
+    }
+    return res;
+  }
 }
